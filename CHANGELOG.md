7d7b6b647008a61c794960c2dbdbabaa0f1c1387
Mathias Hauser <mathause@users.noreply.github.com>
2022-08-05 21:12:37 +0200

    Release v0.1.0 (#28)

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## v0.1.0 - 05.08.2022
+
+- First version released based on the code developped for my IPCC AR6 analyses and including some additions (e.g. `priority_filter`, prefering `kwargs` over keys passed via a dictionary, more complete tests).
--- filefinder/__init__.py
+++ filefinder/__init__.py
@@ -1,4 +1,13 @@
 # flake8: noqa
 
+import pkg_resources
+
 from . import _filefinder, cmip, utils
 from ._filefinder import FileContainer, FileFinder
+
+try:
+    __version__ = pkg_resources.get_distribution("regionmask").version
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "999"
