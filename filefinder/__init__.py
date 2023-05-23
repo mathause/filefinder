ab49e18fe0563dcb526cdc62d1ee05342cc67b7b
Mathias Hauser <mathause@users.noreply.github.com>
2023-05-23 15:28:44 +0200

    update build system (#61)
    * update build system
    
    * remove even more deps

--- filefinder/__init__.py
+++ filefinder/__init__.py
@@ -1,12 +1,18 @@
 # flake8: noqa
 
-import pkg_resources
-
 from . import _filefinder, cmip, utils
 from ._filefinder import FileContainer, FileFinder
 
 try:
-    __version__ = pkg_resources.get_distribution("regionmask").version
+    from importlib.metadata import version as _get_version
+except ImportError:
+    # importlib.metadata not available in python 3.7
+    import pkg_resources
+
+    _get_version = lambda pkg: pkg_resources.get_distribution(pkg).version
+
+try:
+    __version__ = _get_version("filefinder")
 except Exception:
     # Local copy or not installed with setuptools.
     # Disable minimum version checks on downstream libraries.
