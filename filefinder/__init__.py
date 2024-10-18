894eaaf18dc91bbc156ffa40ed57ae26dbcebd30
Mathias Hauser <mathias.hauser@env.ethz.ch>
2024-10-18 15:09:59 +0200

    remove version fallback from coverage

--- filefinder/__init__.py
+++ filefinder/__init__.py
@@ -7,7 +7,7 @@ from ._filefinder import FileContainer, FileFinder
 
 try:
     __version__ = _get_version("filefinder")
-except Exception:
+except Exception:  # pragma: no cover
     # Local copy or not installed with setuptools.
     # Disable minimum version checks on downstream libraries.
     __version__ = "999"
